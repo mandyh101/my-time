c9fceb60668dfd1afd28511312ea7da0cecca94b
mandy <mandyhale10@gmail.com>
2025-04-01 05:59:31 +1300

    tech: install and config CORS to accept requests from trusted origins

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import cors from 'cors'
 
 const app = express();
+
+const corsOptions = {
+  // TODO add live url as CORS trusted origin here too
+  origin: ["http://localhost:5173"]
+}
+
+app.use(cors(corsOptions))
 
 app.get("/api", (req, res) => {
   res.json({"fruits" : ["apple", "orange", "banana"]})
