23264b76b85c8a67ab495fa47b7efabddec218e9
mandy <mandyhale10@gmail.com>
2025-04-01 06:01:39 +1300

    docs: document server set up so far

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import cors from 'cors'
 
+//initialise the app with express
 const app = express();
-
+// set up trusted origins with cors and use cors to verify and receive requests from trusted options only
 const corsOptions = {
   // TODO add live url as CORS trusted origin here too
   origin: ["http://localhost:5173"]
 }
-
 app.use(cors(corsOptions))
-
+//set up api home route
 app.get("/api", (req, res) => {
+  // TODO remove testing
   res.json({"fruits" : ["apple", "orange", "banana"]})
 })
 
+//listen for server connections
 app.listen(8080, () =>{
   console.log("server started on port 8080")
 })
